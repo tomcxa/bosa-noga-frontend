14f8eb1cba13113a1f624153adbf845d27c27d53
tomcxa <tomcxa@yandex.ru>
2020-07-06 14:25:42 +0300

    redirect fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import CatalogPage from "./pages/CatalogPage";
 import HomePage from "./pages/HomePage";
@@ -34,6 +34,7 @@ function App() {
             <Route path="/cart">
               <CartPage />
             </Route>
+            <Redirect from='/bosa-noga-frontend' to='/' >
             <Route exact path="/">
               <HomePage />
             </Route>
